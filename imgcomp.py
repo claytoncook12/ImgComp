1b0bc51e8c13aedb177766c7c0c0e5b3b6310352
Clayton Cook <claytoncook12@gmail.com>
2018-04-26 09:12:18 -0400

    added basic gui for file selection

--- imgcomp.py
+++ imgcomp.py
@@ -53,34 +53,31 @@ def long(dt):
 
     return d + (m/60.0) + (s/3600.0)
 
-if __name__ == "__main__":
-    #imageLocal = "IMG_20180331_142343.jpg"
-    imageLocal = easygui.fileopenbox(msg="Select image you want to compress?",
-                                     title="Title Is Here")
+def compress(fn,qual=70,cfn=True):
+    """Compress Image with Pillow module
+    If cfn is True will add _comp extension to file name
+    """
 
-    exif = get_exif(imageLocal)
-    
-    print("Image lat data display before file size reduction:")
-    print(exif["GPSInfo"]["GPSLatitude"])
-    print("Lat data is: ",lat(exif))
+    # file path, file name and file ending
+    fnP,fn = os.path.split(fn)
+    fnE = fn.split(".")[-1]
+    fnS = fn.split(".")[0]
 
-    photo = Image.open(imageLocal)
-    print("Dim of photo: ",photo.size)
-    print("Size of photo before compression :",os.stat(imageLocal).st_size)
+    exif = get_exif(fnP + "\\" + fn) # Get exif data
+    photo = Image.open(fnP + "\\" + fn)
     exifBytes = photo.info['exif']
 
-    print("Now compressing image\n")
-    photo.save("IMG_comp.jpg","JPEG",optimize=True,quality=70,exif=exifBytes)
-    photoComp = Image.open("IMG_comp.jpg")
-    print("Dim of photo after compression: ",photoComp.size)
-    print("Size of photo after compression :",os.stat("IMG_comp.jpg").st_size)
-    print("File size reduction is: {0:.2f}\n".format(os.stat("IMG_comp.jpg").st_size /
-          os.stat(imageLocal).st_size))
+    # Save photo
+    if cfn:
+        photo.save(fnP + "\\" + fnS + "_comp." + fnE,"JPEG",optimize=True,quality=qual,exif=exifBytes)
+    else:
+        photo.save(fnP + "\\" + fnS + "." + fnE,"JPEG",optimize=True,quality=qual,exif=exifBytes)        
 
-    exifComp = get_exif("IMG_comp.jpg")
-    print("Image lat data display after file size reduction:")
-    print("Lat data after compression: \n",exifComp["GPSInfo"]["GPSLatitude"])
-    print("Lat data is: ",lat(exifComp))
+if __name__ == "__main__":
+    imageLocal = easygui.fileopenbox(msg="Select image you want to compress?")
+
+    compress(imageLocal)
+
         
 
 
