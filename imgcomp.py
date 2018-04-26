b854a988b098d397ffd72b73347bf67c52f3ead2
Clayton Cook <claytoncook12@gmail.com>
2018-04-26 10:13:55 -0400

    Added message boxes to select files to be compressed

--- imgcomp.py
+++ imgcomp.py
@@ -5,7 +5,7 @@ import os
 from PIL import Image
 from PIL.ExifTags import TAGS
 from PIL.ExifTags import GPSTAGS
-import easygui
+import easygui as eg
 
 def get_exif(fn):
     """Returns Dict of picture exif data with
@@ -55,7 +55,8 @@ def long(dt):
 
 def compress(fn,qual=70,cfn=True):
     """Compress Image with Pillow module
-    If cfn is True will add _comp extension to file name
+    cfn - if True will add _comp extension to file name
+    qual - sets quality of image upon save
     """
 
     # file path, file name and file ending
@@ -74,9 +75,42 @@ def compress(fn,qual=70,cfn=True):
         photo.save(fnP + "\\" + fnS + "." + fnE,"JPEG",optimize=True,quality=qual,exif=exifBytes)        
 
 if __name__ == "__main__":
-    imageLocal = easygui.fileopenbox(msg="Select image you want to compress?")
+    title = 'Image Compress'
 
-    compress(imageLocal)
+    # Show message of welcome
+    eg.msgbox("Welcome! Start Compressing Your Images",image="ViceGripPic.png")
+    
+    # Show message to make selection of one image, mutiple images, or folder location
+    selection = eg.choicebox(title=title,
+                 msg="Please select what you want to compress",
+                 choices=['One File','Multiple Files','Folder Location'])
+
+    # Show message box based on previous selection
+    if selection == 'One File':
+        image = eg.fileopenbox(title=title,
+                               msg="Select image you want to compress")
+    elif selection == "Multiple Files":
+        imageLs = eg.fileopenbox(title=title,
+                                 msg="Select images you want to compress",
+                                 multiple=True)
+    elif selection == "Folder Location":
+        path = eg.diropenbox(title=title,
+                             msg="Selection folder location where images will be compressed")
+        
+    # Show message boxes indicating if files renamed
+    paraRename = eg.boolbox(msg="Rename files with new _comp extension?")
+    
+    # Ask if selection is correct for renaming files
+    if paraRename:
+        tell = "want to rename files."
+    else:
+        tell = "don't want to rename files."
+        
+    sure = eg.boolbox(title=title,
+                      msg="Confirm that you " + tell)
+
+    # Go About compressing images
+    
 
         
 
